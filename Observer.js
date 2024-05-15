b450245a7ff48ae56a51a483f7bd0815ecee8c1e
XZ361 <2457307263@qq.com>
2024-05-15 17:44:37 +0800

    feat:add Dep&Watcher

--- Observer.js
+++ Observer.js
@@ -1,3 +1,40 @@
+class Watcher{
+  constructor(expr,vm,cb){
+    this.vm = vm;
+    this.expr = expr;
+    this.cb = cb;
+    // 先将旧值保存起来
+    this.oldVal = getOldVal();
+  }
+  getOldVal(){
+    Dep.target = this;//将watcher和Dep做关联
+    const oldVal = compileUtil.getVal(this.expr,this.vm);
+    Dep.target = null;
+
+    return oldVal;
+  }
+  update(){
+    const newVal = compileUtil.getVal(this.expr,this.vm);
+    if(newVal!==this.oldVal){
+      this.cb(newVal);
+    }
+  }
+}
+class Dep{
+  // 依赖收集器
+  constructor(){
+    this.subs=[];
+  }
+  // 收集观察者
+  addSub(watcher){
+    // 添加订阅者
+    this.subs.push(watcher)
+  }
+  notify(){
+    // 遍历通知每个Watcher,执行更新回调
+    this.subs.forEach(sub=>sub.update())
+  }
+}
 class Observer{
     constructor(data){
         this.observe(data)
@@ -14,11 +51,13 @@ class Observer{
     defineReactive(obj,key,value){
       // 劫持嵌套数据
       this.observe(value);
+      const dep = new Dep();
       Object.defineProperty(obj,key,{
         enumerable:true,
         configurable:false,
         get(){
           // 订阅数据时，向Dep中添加Watcher
+          Dep.target && dep.addSub(Dep.target)
           return value
         },
         set:(newValue)=>{
@@ -27,6 +66,8 @@ class Observer{
           if(newValue!==value){
             value=newValue
           }
+          // 告诉Dep,通知Watcher
+          dep.notify();
         }
       })
     }
